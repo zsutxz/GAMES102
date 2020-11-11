82e34b9077d1995f47fed75fa61c9d6f22d231ce
tanxz <zsutxz@gmail.com>
2020-11-11 22:23:30 +0800

    作业三提交

--- homeworks/project/src/hw3/Systems/CanvasSystem.cpp
+++ homeworks/project/src/hw3/Systems/CanvasSystem.cpp
@@ -8,17 +8,21 @@
 
 using namespace Ubpa;
 
+#define M_PI 3.1415926f
+
 float PAI(const std::vector<Ubpa::pointf2>& P, int j, float x)
 {
 	int n = P.size();
 	float numerator = 1;
 	float denominator = 1;
+	float ret_v = 1.0f;
 	for (int k = 0; k < n; ++k)
 	{
 		if (j == k)
 		{
 			continue;
 		}
+
 		numerator *= (x - P[k][0]);
 		denominator *= (P[j][0] - P[k][0]);
 	}
@@ -35,104 +39,229 @@ float Polynomial(const std::vector<Ubpa::pointf2>& P, float x)
 	}
 	return sum;
 }
+ImVec2 lagrangeInterpolation(float t, const std::vector<float>& parameterization, const std::vector<Ubpa::pointf2>& points) {
+	int n = points.size();
+	for (size_t i = 0; i < n; i++)
+	{
+		if (t == parameterization[i]) return ImVec2(points[i][0], points[i][1]);
+	}
 
-float Gauss(const std::vector<Ubpa::pointf2>& P, float x)
-{
-	int n = P.size();
-	if (n == 0)
+	float x = 0, y = 0;
+	for (size_t j = 0; j < n; j++)
 	{
-		return 0;
-	}
-	float theta = 100;
-	Eigen::MatrixXf A(n, n);
-	for (int row = 0; row < n; ++row)
-	{
-		for (int col = 0; col < n; ++col)
-		{
-			A(row, col) = (std::exp(-(P[row][0] - P[col][0]) * (P[row][0] - P[col][0]) / (2 * theta * theta)));
+		float l = 1.0;
+		for (size_t i = 0; i < n; i++) {
+			if (j == i) continue;
+			l = l * (t - parameterization[i]) / (parameterization[j] - parameterization[i]);
 		}
+
+		x = x + points[j][0] * l;
+		y = y + points[j][1] * l;
 	}
-	Eigen::VectorXf b(n);
-	for (int i = 0; i < n; ++i)
-	{
-		b(i) = P[i][1];
-	}
-	Eigen::VectorXf a = A.colPivHouseholderQr().solve(b);
-	float result = 0;
-	for (int j = 0; j < n; ++j)
-	{
-		result += a[j] * (std::exp(-(x - P[j][0]) * (x - P[j][0]) / (2 * theta * theta)));
-	}
-	return result;
+
+	return ImVec2(x, y);
 }
 
-float LeastSquares(const std::vector<Ubpa::pointf2>& P, float x, int m)
+//第一种方法
+void UniformParameterization(CanvasData* data, const ImVec2 &origin)
 {
-	int n = P.size();
-	if (n == 0)
+	std::vector<Ubpa::pointf2> temp_pX;
+	std::vector<Ubpa::pointf2> temp_pY;
+
+	std::vector<float> temp_sample_nums;
+	for (int i = 0;i < data->points.size();i++)
 	{
-		return 0;
+		//size个数，从0-1均匀分布
+		float den_num = data->points.size() - 1;
+		Ubpa::pointf2 temp_v = Ubpa::pointf2(i * 1.0f / den_num, data->points[i][0]);
+		temp_pX.push_back(temp_v);
+
+		temp_v = Ubpa::pointf2(i * 1.0f / den_num, data->points[i][1]);
+		temp_pY.push_back(temp_v);
+		temp_sample_nums.push_back(i * 1.0f / den_num);
 	}
-	if (m >= n)
+
+	Eigen::VectorXf temp_draw_nums = Eigen::VectorXf::LinSpaced(1000, 0, 1);
+
+	for (size_t i = 0; i < temp_draw_nums.size(); ++i)
 	{
-		m = n - 1;
+		ImVec2 point = ImVec2(Polynomial(temp_pX, temp_draw_nums[i]), Polynomial(temp_pY, temp_draw_nums[i]));
+
+		data->LagrangeResults.push_back(ImVec2(origin.x + point.x, origin.y + point.y));
 	}
-	Eigen::MatrixXf X(n, m);
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			X(i, j) = std::powf(P[i][0], j);
-		}
-	}
-	Eigen::VectorXf Y(n);
-	for (int i = 0; i < n; ++i)
-	{
-		Y(i) = P[i][1];
-	}
-	Eigen::VectorXf Theta = (X.transpose() * X).inverse() * X.transpose() * Y;
-	//Eigen::VectorXf Theta = X.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(Y);
-	float result = 0;
-	for (int j = 0; j < m; ++j)
-	{
-		result += Theta[j] * std::powf(x, j);
-	}
-	return result;
 }
 
-float RidgetRegression(const std::vector<Ubpa::pointf2>& P, float x, float lamda, int m)
+//第二种方法
+void ChordalParameterization(CanvasData* data, const ImVec2& origin)
 {
-	int n = P.size();
-	if (n == 0)
+	std::vector<Ubpa::pointf2> temp_pX;
+	std::vector<Ubpa::pointf2> temp_pY;
+
+	std::vector<float> temp_sample_nums;
+
+	int num = data->points.size();
+	if (num == 1 || num == 2)
 	{
-		return 0;
-	}
-	if (m >= n)
-	{
-		m = n - 1;
-	}
-	Eigen::MatrixXf X(n, m);
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
+		temp_sample_nums.push_back(1.0f);
+		if (num == 2)
 		{
-			X(i, j) = std::powf(P[i][0], j);
+			temp_sample_nums.push_back(1.0f);
 		}
 	}
-	Eigen::VectorXf Y(n);
-	for (int i = 0; i < n; ++i)
+	else
 	{
-		Y(i) = P[i][1];
+		temp_sample_nums.push_back(0.0f);
+		for (int i = 0; i < num - 1; i++)
+		{
+			float dx = data->points[i + 1][0] - data->points[i][0];
+			float dy = data->points[i + 1][1] - data->points[i][1];
+			float chord = dx * dx + dy * dy;//sqrt(dx * dx + dy * dy);
+			temp_sample_nums.push_back(temp_sample_nums[i] + chord);
+		}
 	}
-	Eigen::MatrixXf I(m, m);
-	I.setIdentity();
-	Eigen::VectorXf Theta = (X.transpose() * X + I * lamda).inverse() * X.transpose() * Y;
-	float result = 0;
-	for (int j = 0; j < m; ++j)
+
+	for (int i = 0;i < data->points.size();i++)
 	{
-		result += Theta[j] * std::powf(x, j);
+		//size个数
+		float den_num = temp_sample_nums[data->points.size()-1];
+		Ubpa::pointf2 temp_v = Ubpa::pointf2(1.0f*temp_sample_nums[i], data->points[i][0]);
+		temp_pX.push_back(temp_v);
+
+		temp_v = Ubpa::pointf2(1.0f * temp_sample_nums[i], data->points[i][1]);
+		temp_pY.push_back(temp_v);
 	}
-	return result;
+
+	Eigen::VectorXf temp_draw_nums = Eigen::VectorXf::LinSpaced(1000, 0, temp_sample_nums[num - 1]);
+
+	for (size_t i = 0; i < temp_draw_nums.size(); ++i)
+	{
+		ImVec2 point = ImVec2(Polynomial(temp_pX, temp_draw_nums[i]), Polynomial(temp_pY, temp_draw_nums[i]));
+
+		data->GaussResults.push_back(ImVec2(origin.x + point.x, origin.y + point.y));
+	}
+}
+
+//第三种
+void CentripetalParameterization(CanvasData* data, const ImVec2& origin)
+{
+	std::vector<float> temp_sample_nums;
+
+	int num = data->points.size();
+	if (num == 1 || num == 2)
+	{
+		temp_sample_nums.push_back(1.0f);
+		if (num == 2)
+		{
+			temp_sample_nums.push_back(1.0f);
+		}
+	}
+	else
+	{
+		temp_sample_nums.push_back(0.0f);
+		for (size_t i = 0; i < num - 1; i++)
+		{
+			float dx = data->points[i + 1][0] - data->points[i][0];
+			float dy = data->points[i + 1][1] - data->points[i][1];
+			float chord = sqrt(dx * dx + dy * dy);
+			temp_sample_nums.push_back(temp_sample_nums[i] + chord);
+		}
+	}
+
+	Eigen::VectorXf T = Eigen::VectorXf::LinSpaced(1000, 0, temp_sample_nums[num-1]);
+	for (size_t i = 0; i < T.size(); i++)
+	{
+		ImVec2 point = lagrangeInterpolation(T[i], temp_sample_nums, data->points);
+		data->LeastSquaresResults.push_back(ImVec2(origin.x + point.x, origin.y + point.y));
+	}
+}
+
+float _getAlpha(int i, const std::vector<Ubpa::pointf2>& points) {
+	float dx, dy;
+	// d_prev
+	dx = points[i][0] - points[i - 1][0];
+	dy = points[i][1] - points[i - 1][1];
+	float d_prev = sqrt(dx * dx + dy * dy);
+
+	// d_next
+	dx = points[i + 1][0] - points[i][0];
+	dy = points[i + 1][1] - points[i][1];
+	float d_next = sqrt(dx * dx + dy * dy);
+
+	// l2
+	dx = points[i + 1][0] - points[i - 1][0];
+	dy = points[i + 1][1] - points[i - 1][1];
+	float l2 = dx * dx + dy * dy;
+
+	float alpha = M_PI - acos((d_prev * d_prev + d_next * d_next - l2 / (2 * d_next * d_prev)));
+
+	return alpha;
+}
+
+//第四种
+void FoleyParameterization(CanvasData* data, const ImVec2& origin)
+{
+	std::vector<float> temp_sample_nums;
+
+	int num = data->points.size();
+
+	if (num == 1 || num == 2)
+	{
+		temp_sample_nums.push_back(1.0f);
+		if (num == 2)
+		{
+			temp_sample_nums.push_back(1.0f);
+		}
+		return;
+	}
+
+	temp_sample_nums.push_back(0.0f);
+	float d_prev = 0, d_next = 0;
+	for (size_t i = 0; i < num - 1; i++)
+	{
+		float dx = data->points[i + 1][0] - data->points[i][0];
+		float dy = data->points[i + 1][1] - data->points[i][1];
+		float chord = sqrt(dx * dx + dy * dy);
+
+		if (i == num - 2) d_next = 0;
+		else {
+			float dx = data->points[i + 2][0] - data->points[i + 1][0];
+			float dy = data->points[i + 2][1] - data->points[i + 1][1];
+
+			d_next = sqrt(dx * dx + dy * dy);
+		}
+
+		float factor = 1.0;
+		if (i == 0)
+		{
+			float theta_next = fminf(M_PI / 2, _getAlpha(i + 1, data->points));
+			factor = 1 + 1.5 * (theta_next * d_next) / (chord + d_next);
+		}
+		else if (i == num - 2) {
+			float theta = fminf(M_PI / 2, _getAlpha(i, data->points));
+			factor = 1 + 1.5 * (theta * d_prev) / (d_prev + chord);
+		}
+		else {
+			float theta = fminf(M_PI / 2, _getAlpha(i, data->points));
+			float theta_next = fminf(M_PI / 2, _getAlpha(i + 1, data->points));
+
+			factor = 1 + 1.5 * (theta * d_prev) / (d_prev + chord) + 1.5 * (theta_next * d_next) / (chord + d_next);
+		}
+
+		temp_sample_nums.push_back(temp_sample_nums[i] + chord * factor);
+
+		d_prev = chord;
+	}
+	for (int i = 0;i < num;i++)
+	{
+		temp_sample_nums[i] = temp_sample_nums[i] / temp_sample_nums[num - 1];
+	}
+
+	Eigen::VectorXf T = Eigen::VectorXf::LinSpaced(100, 0, temp_sample_nums[num - 1]);
+	for (size_t i = 0; i < T.size(); i++)
+	{
+		ImVec2 point = lagrangeInterpolation(T[i], temp_sample_nums, data->points);
+		data->RidgeRegressionResults.push_back(ImVec2(origin.x + point.x, origin.y + point.y));
+	}
 }
 
 void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
@@ -144,7 +273,7 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 		if (ImGui::Begin("Canvas")) {
 			ImGui::Checkbox("Enable grid", &data->opt_enable_grid);
 			ImGui::Checkbox("Enable context menu", &data->opt_enable_context_menu);
-			//ImGui::Text("Mouse Left: click to add points,\nMouse Right: drag to scroll, click for context menu.");
+			ImGui::Text("Mouse Left: click to add points,\nMouse Right: drag to scroll, click for context menu.");
 
 			ImGui::Checkbox("Lagrange", &data->opt_lagrange);
 			ImGui::Checkbox("Gauss", &data->opt_gauss);
@@ -156,17 +285,6 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			ImGui::Checkbox("RidgetRegression", &data->opt_ridge_regression);
 			ImGui::SameLine(200);
 			ImGui::InputFloat("lamda", &data->RidgeRegressionLamda, 0.01, 1, 3);
-
-			// Typically you would use a BeginChild()/EndChild() pair to benefit from a clipping region + own scrolling.
-			// Here we demonstrate that this can be replaced by simple offsetting + custom drawing + PushClipRect/PopClipRect() calls.
-			// To use a child window instead we could use, e.g:
-			//      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));      // Disable padding
-			//      ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(50, 50, 50, 255));  // Set a background color
-			//      ImGui::BeginChild("canvas", ImVec2(0.0f, 0.0f), true, ImGuiWindowFlags_NoMove);
-			//      ImGui::PopStyleColor();
-			//      ImGui::PopStyleVar();
-			//      [...]
-			//      ImGui::EndChild();
 
 			// Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
 			ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
@@ -192,8 +310,6 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			if (is_hovered /*&& !data->adding_line*/ && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
 			{
 				data->points.push_back(mouse_pos_in_canvas);
-				//data->points.push_back(mouse_pos_in_canvas);
-				//data->adding_line = true;
 				float Xmin = 999999;
 				float Xmax = -99999;
 				for (size_t i = 0; i < data->points.size(); ++i)
@@ -211,29 +327,27 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 				data->GaussResults.clear();
 				data->LeastSquaresResults.clear();
 				data->RidgeRegressionResults.clear();
-				for (int x = Xmin - 1; x < Xmax + 2; ++x)
+
+				if (data->points.size() > 1)
 				{
-					data->LagrangeResults.push_back(ImVec2(origin.x + x, origin.y + Polynomial(data->points, x)));
-					data->GaussResults.push_back(ImVec2(origin.x + x, origin.y + Gauss(data->points, x)));
-					data->LeastSquaresResults.push_back(ImVec2(origin.x + x, origin.y + LeastSquares(data->points, x, data->LeastSquaresM)));
-					data->RidgeRegressionResults.push_back(ImVec2(origin.x + x, origin.y + RidgetRegression(data->points, x, data->RidgeRegressionLamda, data->LeastSquaresM)));
+					UniformParameterization(data, origin);
+					ChordalParameterization(data, origin);
+					CentripetalParameterization(data, origin);
+					FoleyParameterization(data, origin);
 				}
+
+				//for (int x = Xmin - 1; x < Xmax + 2; ++x)
+				//{
+				//	data->LagrangeResults.push_back(ImVec2(origin.x + x, origin.y + Polynomial(data->points, x)));
+				//	data->GaussResults.push_back(ImVec2(origin.x + x, origin.y + Gauss(data->points, x)));
+				//	data->LeastSquaresResults.push_back(ImVec2(origin.x + x, origin.y + LeastSquares(data->points, x, data->LeastSquaresM)));
+				//	data->RidgeRegressionResults.push_back(ImVec2(origin.x + x, origin.y + RidgetRegression(data->points, x, data->RidgeRegressionLamda, data->LeastSquaresM)));
+				//}
 			}
-			/*if (data->adding_line)
-			{
-				data->points.back() = mouse_pos_in_canvas;
-				if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
-					data->adding_line = false;
-			}*/
 
 			// Pan (we use a zero mouse threshold when there's no context menu)
 			// You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
 			const float mouse_threshold_for_pan = data->opt_enable_context_menu ? -1.0f : 0.0f;
-			/*if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan))
-			{
-				data->scrolling[0] += io.MouseDelta.x;
-				data->scrolling[1] += io.MouseDelta.y;
-			}*/
 
 			// Context menu (under default mouse threshold)
 			ImVec2 drag_delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Right);
@@ -263,8 +377,6 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			{
 				draw_list->AddCircleFilled(ImVec2(origin.x + data->points[n][0], origin.y + data->points[n][1]), 4.0f, IM_COL32(255, 255, 0, 255));
 			}
-			//for (int n = 0; n < data->points.size(); n += 2)
-				//draw_list->AddLine(ImVec2(origin.x + data->points[n][0], origin.y + data->points[n][1]), ImVec2(origin.x + data->points[n + 1][0], origin.y + data->points[n + 1][1]), IM_COL32(255, 255, 0, 255), 2.0f);
 
 			if (data->opt_lagrange)
 			{
